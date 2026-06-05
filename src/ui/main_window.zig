2ccd85281f38f304a26ac11b5ea03694d62a3671
Jackson Ly <bobjacklee27@gmail.com>
2026-06-06 09:49:24 +1000

    add: exit application menu functionality

--- src/ui/main_window.zig
+++ src/ui/main_window.zig
@@ -14,6 +14,15 @@ const QListWidget = qt6.QListWidget;
 
 const qnamespace_enums = qt6.qnamespace_enums;
 
+const AppWindow = struct {
+    var window: QMainWindow = undefined;
+
+    fn exit_window(action: QAction) callconv(.c) void {
+        _ = action;
+        _ = window.Close();
+    }
+};
+
 pub fn initQtApplication(init: std.process.Init) !void {
     const argv = try qt6.init(init.gpa, init.minimal.args);
     defer qt6.deinit(init.gpa, argv);
@@ -22,29 +31,36 @@ pub fn initQtApplication(init: std.process.Init) !void {
     const qapp = QApplication.New(init.arena.allocator(), &argc, argv);
     defer qapp.Delete();
 
-    const window = QMainWindow.New2();
-    defer window.Delete();
-    window.SetFixedSize2(300, 250);
+    AppWindow.window = QMainWindow.New2();
+    defer AppWindow.window.Delete();
+    AppWindow.window.SetFixedSize2(300, 250);
 
     const widget = QWidget.New2();
     defer widget.Delete();
-    window.SetCentralWidget(widget);
+    AppWindow.window.SetCentralWidget(widget);
 
     const menu_bar = QMenuBar.New2();
-    window.SetMenuBar(menu_bar);
+    AppWindow.window.SetMenuBar(menu_bar);
     menu_bar.SetNativeMenuBar(false);
 
     const file_menu = menu_bar.AddMenu2("Emulator");
-    const exitAction = QAction.New2("Exit");
-    exitAction.SetShortcut(QKeySequence.New2("Ctrl+Q"));
-    file_menu.AddAction(exitAction);
+    const load_rom_action = QAction.New2("Load rom...");
+    file_menu.AddAction(load_rom_action);
+
+    const reset_action = QAction.New2("Reset");
+    file_menu.AddAction(reset_action);
+
+    const exit_action = QAction.New2("Exit");
+    exit_action.SetShortcut(QKeySequence.New2("Ctrl+Q"));
+    exit_action.OnTriggered(AppWindow.exit_window);
+    file_menu.AddAction(exit_action);
 
     const tools_menu = menu_bar.AddMenu2("Tools");
-    const traceloggerAction = QAction.New2("Tracelogger");
+    const tracelogger_action = QAction.New2("Tracelogger");
     // traceloggerAction.OnTriggered();
-    tools_menu.AddAction(traceloggerAction);
+    tools_menu.AddAction(tracelogger_action);
 
-    window.Show();
+    AppWindow.window.Show();
 
     _ = QApplication.Exec();
 }
