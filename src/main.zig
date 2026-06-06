0fe525a74481a816af96df96b4c5bd0b23ca1ab2
Jackson Ly <bobjacklee27@gmail.com>
2026-06-06 10:48:52 +1000

    chore: clean up main

--- src/main.zig
+++ src/main.zig
@@ -4,14 +4,6 @@ const emulator = @import("emulator.zig");
 const gui = @import("ui/main_window.zig");
 
 pub fn main(init: std.process.Init) !void {
-    // const args = try init.minimal.args.toSlice(arena.allocator());
-    // if (args.len < 2) return error.ExpectedArgument;
-
-    // const rom_path = args[1];
-
-    // try emulator.reset(io, rom_path);
-    // try emulator.run();
-
     try gui.initQtApplication(init);
 }
 
